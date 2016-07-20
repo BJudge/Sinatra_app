aa4346b1c2c065c66062f33fb16fc14c6b684b97
BartJudge <BartJudge@gmail.com>
2016-07-20 14:12:05 +0100

    exercise 14 PSOT method and forms

--- app.rb
+++ app.rb
@@ -14,9 +14,14 @@ class Webserver < Sinatra::Base
     erb(:index)
   end
 
-  get '/named-cat' do
+  post '/named-cat' do
     p params
     @name = params[:name]
     erb(:index)
   end
+
+  get '/cat-form' do
+    erb(:cat_form)
+  end
+
 end
